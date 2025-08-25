1e1f16bcb169cd966456383b64444f0658d6cc1f
carolinaporto <mariacp9@al.insper.edu.br>
2025-08-24 21:11:50 -0300

    ex0, ex1, ex2, ex3

--- exe2/main.c
+++ exe2/main.c
@@ -2,9 +2,44 @@
 #include "pico/stdlib.h"
 #include <stdio.h>
 
+const int BTN_PIN_R = 28;
+const int LED = 4;
+
+volatile int rise_flag = 0;
+volatile int fall_flag = 0;
+
+void btn_callback(uint gpio, uint32_t events) {
+  if (events == 0x4) { // fall edge
+    fall_flag = 1;
+  } else if (events == 0x8) { // rise edge
+    rise_flag = 1;
+  }
+}
+
 int main() {
   stdio_init_all();
 
+  gpio_init(BTN_PIN_R);
+  gpio_set_dir(BTN_PIN_R, GPIO_IN);
+  gpio_pull_up(BTN_PIN_R);
+
+  gpio_init(LED);
+  gpio_set_dir(LED, GPIO_OUT);
+
+  gpio_set_irq_enabled_with_callback(
+      BTN_PIN_R, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &btn_callback);
+
   while (true) {
+    if (rise_flag){
+      printf("rise \n");
+      gpio_put(LED, 0);
+      rise_flag = 0;
+    }
+    if (fall_flag){
+      printf("fall \n");
+      gpio_put(LED, 1);
+      fall_flag = 0;
+    }
+    sleep_ms(200);
   }
 }
--- exe3/main.c
+++ exe3/main.c
@@ -6,12 +6,15 @@
 const int BTN_PIN_R = 28;
 const int BTN_PIN_G = 26;
 
+volatile int red_flag = 0;
+volatile int green_flag = 0;
+
 void btn_callback(uint gpio, uint32_t events) {
     if (events == 0x4) {  // fall edge
         if (gpio == BTN_PIN_R) {
-            printf("fall red\n");
+           red_flag = 1;
         } else if (gpio == BTN_PIN_G) {
-            printf("fall green\n");
+            green_flag = 1;
         }
     }
 }
@@ -35,5 +38,14 @@ int main() {
     gpio_set_irq_enabled(BTN_PIN_G, GPIO_IRQ_EDGE_FALL, true);
 
     while (true) {
+        if (red_flag){
+            printf("fall red \n");
+            red_flag = 0;
+        }
+
+        if (green_flag){
+            printf("fall green \n");
+            green_flag = 0;
+        }
     }
 }
